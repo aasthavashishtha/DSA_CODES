6293d5598afcf643442cf9ee24b07a0e2329b5c7
aasthavashishtha <142791361+aasthavashishtha@users.noreply.github.com>
2024-01-06 17:16:25 +0530

    Update and rename main (6).cpp to pascal's_theorem.cpp

--- pascal's_theorem.cpp
+++ pascal's_theorem.cpp
@@ -1,4 +1,10 @@
-
+/* pascal triangle 
+    1                                0C0
+    1    2                           1C0    1C1
+    1    2    1                      2C0    2C1    2C2
+    1    3    3    1                 3C0    3C1    3C2    3C3
+    1    4    6    4    1            4C0    4C1    4C2    4C3    4C4
+*/
 #include<iostream>
 #include<math.h>
 using namespace std;
